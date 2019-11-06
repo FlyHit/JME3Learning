62bd9c95889fcc961a39b68b4c45b24ce64d2095
FlyHit <2692217528@qq.com>
2019-11-06 09:51:49 +0800

    添加滚轮输入及覆写第一人称控制器

--- InputSystem/src/jme3test/helloworld/HelloInput.java
+++ InputSystem/src/jme3test/helloworld/HelloInput.java
@@ -6,6 +6,7 @@ import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseAxisTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
@@ -42,20 +43,24 @@ public class HelloInput extends SimpleApplication {
      * 自定义按键绑定：将已命名的动作映射到某输入
      */
     private void initKeys() {
-        flyCam.setDragToRotate(true);
+        flyCam.setEnabled(false); // 关闭第一人称视角的控制器
         inputManager.setCursorVisible(true);
-        
+
         // 将一个或多个输入映射到已命名的动作（注册映射名及其触发器）
         inputManager.addMapping("Pause", new KeyTrigger(KeyInput.KEY_P));
-        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_J));
-        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_K));
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_J), new KeyTrigger(keyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_K), new KeyTrigger(keyInput.KEY_D));
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_H), new KeyTrigger(keyInput.KEY_W),
+                new MouseAxisTrigger(MouseInput.AXIS_WHEEL, false));  // false：向上滚动
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_L), new KeyTrigger(keyInput.KEY_S),
+                 new MouseAxisTrigger(MouseInput.AXIS_WHEEL, true));  // true：向下滚动
         inputManager.addMapping("Rotate", new KeyTrigger(KeyInput.KEY_SPACE),
                 new MouseButtonTrigger(MouseInput.BUTTON_LEFT)); // 可以是多个
         inputManager.addMapping("Move", new MouseButtonTrigger(MouseInput.BUTTON_MIDDLE)); // 用鼠标中键移动
-        
+
         // 将动作名添加到动作监听器（注册上面的触发器映射）
         inputManager.addListener(actionListener, "Pause"); // actionListener：用于接收数字量输入的事件
-        inputManager.addListener(analogListener, "Left", "Right", "Rotate","Move"); // analogListener：用于接收模拟量输入的事件
+        inputManager.addListener(analogListener, "Left", "Right", "Up", "Down", "Rotate", "Move"); // analogListener：用于接收模拟量输入的事件
 //        除了使用ActionListener和AnalogListener外，还可以自定义CombinedListener（同时实现ActionListener和AnalogListener，
 //        可接收两种类型的事件）
 //        inputManager.addListener(combinedListener, new String[]{"Pause", "Left", "Right", "Rotate"});
@@ -86,8 +91,17 @@ public class HelloInput extends SimpleApplication {
                 Vector3f v = player.getLocalTranslation();
                 player.setLocalTranslation(v.x - value * speed, v.y, v.z);
             }
-            if(name.equals("Move")){
-                player.move(Mouse.getDX()*tpf, Mouse.getDY()*tpf, 0);
+            if (name.equals("Up")) {
+                Vector3f v = player.getLocalTranslation();
+                player.setLocalTranslation(v.x, v.y + value * speed, v.z);
+            }
+            if (name.equals("Down")) {
+                Vector3f v = player.getLocalTranslation();
+                player.setLocalTranslation(v.x, v.y - value * speed, v.z);
+            }
+
+            if (name.equals("Move")) {
+                player.move(Mouse.getDX() * tpf, Mouse.getDY() * tpf, 0);
                 System.out.println(Mouse.getDX());
             }
         } else {
